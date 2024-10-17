d5fa1537de2968d80ce1f2f347c6f12bc034e850
Erikangu <ssdhanush17@gmail.com>
2024-10-17 14:17:05 +0530

    Added pages(-functionality)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//pages
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+
+export default function App(){
+  return <Router>
+    <Routes>
+      <Route path = '/' element = {<Home />} />
+      <Route path = '/login' element = {<Login />} />
+      <Route path = '/profile' element = {<Profile />} />
+      <Route path = '/register' element = {<Register />} />
+    </Routes>
+  </Router>
 }
